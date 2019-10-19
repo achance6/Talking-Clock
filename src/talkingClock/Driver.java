10ee00b3154f06eb04787863170174a28f85248f
achance6 <achance6@jhu.edu>
2019-10-19 17:56:33 -0400

    initial commit

---
+++ src/talkingClock/Driver.java
@@ -0,0 +1,12 @@
+package talkingClock;
+
+public final class Driver {
+    private Driver() {
+    }
+    /** Main method.
+     * @param args not used.
+     */
+    public static void main(final String[] args) {
+        
+    }
+}
